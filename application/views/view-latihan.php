21a4543a73150d97aef082d87932859f8069ccf9
Afif701 <afifakramul1@gmail.com>
2024-03-28 12:45:34 +0700

    upload contoh 5

---
+++ application/views/view-latihan.php
@@ -0,0 +1,16 @@
+<html>
+    
+<head>
+    <title>Latihan1</title>
+</head>
+
+<body>
+    Hii Teman.. Yuk kita belajar web programming..!!!<br>
+    Nilai 1 = <?= $nilai1; ?>
+    Nilai 2 = <?= $nilai2; ?>
+    ini hasil dari penjumlahan dengan methode penjumlahan yaitu <?=
+$nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+
+</body>
+
+</html>
