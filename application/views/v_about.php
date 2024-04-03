ab53fdaefc77d5a05217e2fcbfd0094846f4e341
Afif701 <afifakramul1@gmail.com>
2024-04-03 13:05:23 +0700

    upload

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Afif</li>
+        <li>Nama Belakang : Akramul</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li>Jl.Margonda Raya Griya Salak No.30, Beji Depok</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Depok</li>
+    </ul>
+
+    <h4>Olahraga Favorit</h4>
+    <ul type="square">
+        <li>Catur</li>
+    </ul>
+</section>
